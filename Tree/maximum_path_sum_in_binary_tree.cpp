59f901ada7d15b29e2adceedd43f6a0c45dbb952
Saurav4742 <sauravpatel668@gmail.com>
2024-10-01 22:33:34 +0530

    first commit

---
+++ Tree/maximum_path_sum_in_binary_tree.cpp
@@ -0,0 +1,61 @@
+// Maximum Path Sum in a Binary Tree
+
+// Given a binary tree, find the maximum path sum. The path may start and end at any node in the tree.
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* newNode = new Node;
+    newNode->data = data;
+    newNode->left= newNode->right = NULL;
+    return (newNode);
+}
+
+int findMaxUtil(Node* root, int& res) {
+
+    if (root == NULL)
+        return 0;
+
+    int l= findMaxUtil(root->left,res);
+    int r= findMaxUtil(root->right,res);
+
+    int max_single = max(max(l,r) + root->data,root->data);
+
+    int max_top = max(max_single, l+r+root->data);
+
+    res = max(res, max_top);
+
+    return max_single;
+}
+
+int findMaxSum(Node* root){
+    int res = INT_MIN; //initilise result
+
+    findMaxUtil(root,res); //compute and return result
+    return res;
+}
+
+int32_t main(){
+    struct Node* root = newNode(10);
+    root->left = newNode(2);
+    root->right = newNode(10);
+    root->left->left = newNode(20);
+    root->left->right = newNode(1);
+    root->right->right = newNode(-25);
+    root->right->right->left = newNode(3);
+    root->right->right->right = newNode(4);
+ 
+    // Function call
+    cout << "Max path sum is " << findMaxSum(root);
+
+    return 0;
+}
